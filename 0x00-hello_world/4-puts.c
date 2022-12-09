264f98dccfc115c4466c598ba3d50930afe044db
DreKip <andkiru89@gmail.com>
2022-12-09 10:26:34 -0800

    No 4 and 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 
-/**
- * Main - Entry Point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
-
-
